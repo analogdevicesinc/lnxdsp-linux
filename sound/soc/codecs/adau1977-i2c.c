tymphany/adi-linux-kernel

sound/soc/codecs: Adjust ADAU1977 driver for Analog Devices ADSP-SC5XX architecture
Co-authored-by: Greg Malysa <greg.malysa@timesys.com>

sound/soc/codecs/adau1977-i2c.c
@@ -28,10 +28,27 @@ static int adau1977_i2c_probe(struct i2c_client *client,
 		id->driver_data, NULL);
 }
 
+static int adau1977_i2c_remove(struct i2c_client *client)
+{
+	// DBB snd_soc_unregister_ card codec (&client->dev);
+	adau1977_remove(&client->dev);
+	return 0;
+}
+
+#ifdef CONFIG_OF
+static const struct of_device_id adau1977_dt_ids[] = {
+	{ .compatible = "adi,adau1977", .data=(const void *)ADAU1977 },
+	{ .compatible = "adi,adau1978", .data=(const void *)ADAU1978 },
+	{ .compatible = "adi,adau1979", .data=(const void *)ADAU1979 },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, adau1977_dt_ids);
+#endif
+
 static const struct i2c_device_id adau1977_i2c_ids[] = {
 	{ "adau1977", ADAU1977 },
 	{ "adau1978", ADAU1978 },
-	{ "adau1979", ADAU1978 },
+	{ "adau1979", ADAU1979 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, adau1977_i2c_ids);
@@ -39,8 +56,10 @@ MODULE_DEVICE_TABLE(i2c, adau1977_i2c_ids);
 static struct i2c_driver adau1977_i2c_driver = {
 	.driver = {
 		.name = "adau1977",
+		.of_match_table = of_match_ptr(adau1977_dt_ids),
 	},
 	.probe = adau1977_i2c_probe,
+	.remove = adau1977_i2c_remove,
 	.id_table = adau1977_i2c_ids,
 };
 module_i2c_driver(adau1977_i2c_driver);

analogdevicesinc/lnxdsp-linux

Co-developed-by: Greg Malysa <greg.malysa@timesys.com>
Signed-off-by: Greg Malysa <greg.malysa@timesys.com>
Signed-off-by: Nathan Barrett-Morrison <nathan.morrison@timesys.com>

sound/soc/codecs/adau1977-i2c.c
@@ -28,10 +28,27 @@ static int adau1977_i2c_probe(struct i2c_client *client,
 		id->driver_data, NULL);
 }
 
+static int adau1977_i2c_remove(struct i2c_client *client)
+{
+	// DBB snd_soc_unregister_ card codec (&client->dev);
+	adau1977_remove(&client->dev);
+	return 0;
+}
+
+#ifdef CONFIG_OF
+static const struct of_device_id adau1977_dt_ids[] = {
+	{ .compatible = "adi,adau1977", .data = (const void *)ADAU1977 },
+	{ .compatible = "adi,adau1978", .data = (const void *)ADAU1978 },
+	{ .compatible = "adi,adau1979", .data = (const void *)ADAU1979 },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, adau1977_dt_ids);
+#endif
+
 static const struct i2c_device_id adau1977_i2c_ids[] = {
 	{ "adau1977", ADAU1977 },
 	{ "adau1978", ADAU1978 },
-	{ "adau1979", ADAU1978 },
+	{ "adau1979", ADAU1979 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, adau1977_i2c_ids);
@@ -39,8 +56,10 @@ MODULE_DEVICE_TABLE(i2c, adau1977_i2c_ids);
 static struct i2c_driver adau1977_i2c_driver = {
 	.driver = {
 		.name = "adau1977",
+		.of_match_table = of_match_ptr(adau1977_dt_ids),
 	},
 	.probe = adau1977_i2c_probe,
+	.remove = adau1977_i2c_remove,
 	.id_table = adau1977_i2c_ids,
 };
 module_i2c_driver(adau1977_i2c_driver);